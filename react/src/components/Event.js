5574f1d4c69ef3ffde08c4661e9474916533abfe
jack <lightjp@cardiff.ac.uk>
2020-02-08 12:14:43 +0000

    Add event title, desc and image

--- react/src/components/Event.js
+++ react/src/components/Event.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Event extends Component {
-	render = () => <div><p>This is the event component</p></div>
+	render = () => <div>
+		<p>{this.props.title}</p>
+		<p>{this.props.description}</p>
+		<img src={this.props.image} alt={'Event image'} />
+	</div>
 }
+
+Event.propTypes = {
+	title: PropTypes.string,
+	description: PropTypes.string,
+	image: PropTypes.string,
+};
 
 export default Event;
