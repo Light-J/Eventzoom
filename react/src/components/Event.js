d5cb5a0a4748a44517e4583fbf9c88ef0a9c3d2a
jack <lightjp@cardiff.ac.uk>
2020-02-08 12:32:53 +0000

    Add all event details to component

--- react/src/components/Event.js
+++ react/src/components/Event.js
@@ -6,6 +6,12 @@ class Event extends Component {
 		<p>{this.props.title}</p>
 		<p>{this.props.description}</p>
 		<img src={this.props.image} alt={'Event image'} />
+		<p>{this.props.datetime.toDateString()}</p>
+		<p>{this.props.speaker}</p>
+		<p>{this.props.vagueLocation}</p>
+		<p>{this.props.disabilityAccess}</p>
+		<p>{this.props.organiser}</p>
+		<p>{this.props.curAttending} out of {this.props.capacity}</p>
 	</div>
 }
 
@@ -13,6 +19,13 @@ Event.propTypes = {
 	title: PropTypes.string,
 	description: PropTypes.string,
 	image: PropTypes.string,
+	datetime: PropTypes.instanceOf(Date),
+	speaker: PropTypes.string,
+	vagueLocation: PropTypes.string,
+	disabilityAccess: PropTypes.bool,
+	organiser: PropTypes.string,
+	curAttending: PropTypes.number,
+	capacity: PropTypes.number,
 };
 
 export default Event;
--- react/src/containers/App.js
+++ react/src/containers/App.js
@@ -8,7 +8,7 @@ import Event from '../components/Event';
 class App extends Component {
 	static propTypes = {
 		isLoggedIn: PropTypes.bool.isRequired,
-	}
+	};
 
 	render = () => <div>
 		<NavBar />
@@ -16,7 +16,18 @@ class App extends Component {
 				Hello World! { this.isLoggedIn ? 'yes' : 'no'}
 		</Route>
 		<Route exact path="/event" >
-			<Event />
+
+			<Event
+				title={'Cat-a-Burger?'}
+				description={'Cats like burgers?? Come to this talk where we talk all about the different wonders of a cats diet'}
+				image={'https://i.imgur.com/vgZrB5U.jpg'}
+				datetime={new Date('03-01-2020')}
+				speaker={'John Smith'}
+				organiser={'Smith John'}
+				vagueLocation={'Cardiff'}
+				disabilityAccess={false}
+				curAttending={50} capacity={55}
+			/>
 		</Route>
 	</div>
 }
