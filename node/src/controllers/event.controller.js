f81be217561677a09f71da17c489647a87e02942
jack <lightjp@cardiff.ac.uk>
2020-02-12 14:21:44 +0000

    Simple event returned from controller temporarily

--- node/src/controllers/event.controller.js
+++ node/src/controllers/event.controller.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import Event from '../models/event.model';
 
 const router = express.Router();
 
 router.get(
 	'/',
 	(req, res) => {
-		res.send('Event details');
+		const event = new Event({
+			title: 'Cats!',
+			description: 'This is the description of my test event',
+			image: 'https://i.imgur.com/ZccahuC.jpg',
+			speaker: 'John cat-a-person',
+			location: 'Cardiff',
+			disabilityaccess: true,
+			organiser: 'Catty catface',
+			capacity: 50,
+			date: new Date('03-03-2020'),
+		});
+		res.send(event);
 	},
 );
 
