900e2587de867666f82445705b58b41b3e4f85f2
jack <lightjp@cardiff.ac.uk>
2020-02-12 12:53:58 +0000

    Add todo to place events list results

--- react/src/components/SearchResults.js
+++ react/src/components/SearchResults.js
@@ -3,6 +3,7 @@ import SearchResult from './SearchResult';
 
 class SearchResults extends Component {
 	render = () => <div>
+		{/* TODO populate this with the results from DB instead */}
 		<SearchResult image="https://i.imgur.com/Ka4mk2q.jpg" title="Should we feed cats?" author="James"/>
 		<SearchResult image="https://i.imgur.com/Jyr2QUc.jpg" title="Are cats evil?" author="Not-a-cat"/>
 		<SearchResult image="https://i.imgur.com/xw4GsA6.jpg" title="What is a cat, really?" author="Catty McCattyson" />
