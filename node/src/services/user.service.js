20da0ddac96159620a38d0d3f9217b3684879ac6
Hristo <dimitrovhd@cardiff.ac.uk>
2020-02-14 19:52:49 +0000

    + hashing

--- node/src/services/user.service.js
+++ node/src/services/user.service.js
@@ -1,7 +1,8 @@
+import bcrypt from 'bcryptjs';
 import User from '../models/user.model';
 
 const createUser = async (user) => {
-	await ((new User(user)).save());
+	await ((new User({ ...user, password: bcrypt.hashSync(user.password, 8) })).save());
 	return true;
 };
 
