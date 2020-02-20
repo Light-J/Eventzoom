c25435d6a50dbdc693f7ae435e0438c8e806970a
Calum Mcalinden <calum@mcalinden.me.uk>
2020-02-20 17:55:22 +0000

    Pass auth token with axios requests

--- react/src/index.js
+++ react/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './containers/App';
 import configureStore from './store/store';
+
+axios.interceptors.request.use((config) => {
+	// eslint-disable-next-line no-param-reassign
+	config.headers.Authorization = `Bearer ${localStorage.getItem('JWT')}`;
+	return config;
+});
 
 const store = configureStore();
 ReactDOM.render(
