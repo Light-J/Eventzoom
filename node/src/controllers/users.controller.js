435a3671c8f7310215551799a4b6ac9e5cea3fdc
Calum Mcalinden <calum@mcalinden.me.uk>
2020-02-21 12:17:50 +0000

    Corrected issues

--- node/src/controllers/users.controller.js
+++ node/src/controllers/users.controller.js
@@ -1,58 +1,8 @@
 import express from 'express';
-import passport from 'passport';
-import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
-import jwt from 'jsonwebtoken';
-import passportLocal from 'passport-local';
 import validator from '../middleware/validator';
-import UserService from '../services/user.service';
 import User from '../models/user.model';
 import isAuthenticated from '../middleware/isAuthenticated';
-
-passport.serializeUser((user, done) => {
-	done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-	done(null, user);
-});
-
-passport.use(new passportLocal.Strategy({
-	usernameField: 'username',
-	passwordField: 'password',
-	session: false,
-},
-(async (username, password, done) => {
-	const user = await UserService.getUserByEmail(username);
-	if (!user) { return done(null, false); }
-	if (!await user.verifyPassword(password)) { return done(null, false); }
-	return done(null, user);
-})));
-
-const JwtOptions = {
-	jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('JWT'),
-	secretOrKey: process.env.SESSION_SECRET || 'cats',
-};
-
-passport.use(
-	'jwt',
-	new JwtStrategy(JwtOptions, (jwtPayload, done) => {
-		try {
-			User.findOne({
-				where: {
-					id: jwtPayload.id,
-				},
-			}).then((user) => {
-				if (user) {
-					done(null, user);
-				} else {
-					done(null, false);
-				}
-			});
-		} catch (err) {
-			done(err);
-		}
-	}),
-);
+import userService from '../services/user.service';
 
 const router = express.Router();
 
@@ -70,20 +20,16 @@ router.post(
 );
 
 router.get('/me', isAuthenticated, async (req, res) => {
-	const user = await UserService.getUserById(req.validated);
-	res.json({ user });
+	res.json({ name: req.user.name });
 });
 
 router.post(
 	'/login',
 	passport.authenticate('local'),
 	async (req, res) => {
-		const token = jwt.sign({ id: req.user.id }, JwtOptions.secretOrKey, {
-			expiresIn: 60 * 60,
-		});
 		res.json({
 			success: true,
-			token,
+			token: userService.getJwtToken(),
 		});
 	},
 );
--- node/src/services/user.service.js
+++ node/src/services/user.service.js
@@ -1,4 +1,9 @@
 import bcrypt from 'bcryptjs';
+import passport from 'passport';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import jwt from 'jsonwebtoken';
+import passportLocal from 'passport-local';
+import authConfig from '../../config/auth';
 import User from '../models/user.model';
 
 const createUser = async (user) => {
@@ -23,5 +28,49 @@ const getUserById = async (id) => {
 	}
 };
 
+const JwtOptions = {
+	jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('JWT'),
+	secretOrKey: authConfig.key,
+};
 
-export default { createUser, getUserByEmail, getUserById };
+const getJwtToken = async () => jwt.sign({ id: req.user.id }, JwtOptions.secretOrKey, {
+	expiresIn: 60 * 60,
+});
+
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.use(new passportLocal.Strategy({
+	session: false,
+},
+(async (username, password, done) => {
+	const user = await getUserByEmail(username);
+	if (!user) { return done(null, false); }
+	if (!await user.verifyPassword(password)) { return done(null, false); }
+	return done(null, user);
+})));
+
+passport.use(
+	'jwt',
+	new JwtStrategy(JwtOptions, async (jwtPayload, done) => {
+		try {
+			const user = await getUserById(jwtPayload.id);
+			if (user) {
+				done(null, user);
+			} else {
+				done(null, false);
+			}
+		} catch (err) {
+			done(err);
+		}
+	}),
+);
+
+export default {
+	createUser, getUserByEmail, getUserById, getJwtToken,
+};
