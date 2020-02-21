c64dff809aa3097984cdfd5bb7d13c09585bed33
Light <Lightjp@cardiff.ac.uk>
2020-02-21 17:04:27 +0000

    Fix test name

--- react/src/tests/components/DisabilityAccess.test.js
+++ react/src/tests/components/DisabilityAccess.test.js
@@ -10,7 +10,7 @@ describe('Tests loading of disability access', () => {
 		const wrapper = Enzyme.shallow(<DisabilityAccess disabilityAccess={false} />);
 		expect(wrapper.contains(<div className="alert alert-danger" role="alert">This event unfortunatly does not have disability access</div>)).toEqual(true);
 	});
-	it('Sets access to none if given false', async () => {
+	it('Sets access to available if given true', async () => {
 		const wrapper = Enzyme.shallow(<DisabilityAccess disabilityAccess={true} />);
 		expect(wrapper.contains(<div className="alert alert-success" role="alert">This event has disability access</div>)).toEqual(true);
 	});
