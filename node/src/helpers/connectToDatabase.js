5eae4eca07b0aef47a68ddbe8421ef2417ba3010
Hristo <dimitrovhd@cardiff.ac.uk>
2020-02-22 08:01:36 +0000

    wip

--- node/src/helpers/connectToDatabase.js
+++ node/src/helpers/connectToDatabase.js
@@ -1,4 +1,4 @@
 import Mongoose from 'mongoose';
 import databaseConfig from '../../config/database';
-
+console.log(databaseConfig.uri);
 Mongoose.connect(databaseConfig.uri, { useNewUrlParser: true, useUnifiedTopology: true });
