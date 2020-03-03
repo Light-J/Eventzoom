11684d82473d7ab59f5d2e960393fafa0db02996
Calum Mcalinden <calum@mcalinden.me.uk>
2020-03-03 14:26:55 +0000

    Updated tests

--- node/tests/middleware/validator.test.js
+++ node/tests/middleware/validator.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-classes-per-file */
 import validator from '../../src/middleware/validator';
+import bcryptjs from "bcryptjs";
 
 // taken from https://gitlab.cs.cf.ac.uk/c1734384/react-assessment-1/blob/master/assessment-1-server/tests/middleware/validation.test.js
 describe('testing required', () => {
@@ -102,25 +103,25 @@ describe('testing correctPassword', () => {
 	it('should succeed if correct', async () => {
 		const req = { body: { password: 'hello' }, user: { password: '$2a$08$3l7YO0UVu3u.xHOspgokQufV9YJYRblkNgfTFmXbHn/L4MdpVHZEK' } };
 		const res = { status: jest.fn(), json: jest.fn() };
+		bcryptjs.compare = jest.fn().mockImplementation(async () => true);
 		const next = jest.fn();
 		await validator('correctPassword', { field: 'password' })(req, res, next);
 		expect(req.validated.password).toEqual('hello');
-		expect(next.mock.calls.length).toEqual(1);
 	});
 	it('should fail if not correct', async () => {
 		const req = { body: { password: 'xxx' }, user: { password: '$2a$08$3l7YO0UVu3u.xHOspgokQufV9YJYRblkNgfTFmXbHn/L4MdpVHZEK' } };
 		const res = { status: jest.fn(), json: jest.fn() };
+		bcryptjs.compare = jest.fn().mockImplementation(async () => false);
 		const next = jest.fn();
 		await validator('correctPassword', { field: 'password' })(req, res, next);
 		expect(req.validated.password).toEqual(undefined);
-		expect(next.mock.calls.length).toEqual(1);
 	});
 	it('should fail if null', async () => {
 		const req = { body: { }, user: { password: '$2a$08$3l7YO0UVu3u.xHOspgokQufV9YJYRblkNgfTFmXbHn/L4MdpVHZEK' } };
 		const res = { status: jest.fn(), json: jest.fn() };
+		bcryptjs.compare = jest.fn().mockImplementation(async () => false);
 		const next = jest.fn();
 		await validator('correctPassword', { field: 'password' })(req, res, next);
 		expect(req.validated.password).toEqual(undefined);
-		expect(next.mock.calls.length).toEqual(1);
 	});
 });
