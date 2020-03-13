255ce13d9dd2b69b2fd8b6af2c19077f4c113d36
Light <Lightjp@cardiff.ac.uk>
2020-03-13 18:02:22 +0000

    Reset slide back to 0 using the carousel store

--- react/src/components/SeriesResults.js
+++ react/src/components/SeriesResults.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-	Slider, Slide, ButtonBack, ButtonNext, WithStore,
+	Slider, Slide, WithStore,
 } from 'pure-react-carousel';
-import Conditional from './Conditional';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import SeriesResult from './SeriesResult';
-import classes from './SeriesResult.module.css';
 
 class SeriesResults extends React.Component {
 	static propTypes = {
@@ -16,11 +14,6 @@ class SeriesResults extends React.Component {
 		setStoreState: PropTypes.func,
 	};
 
-	state = {
-		width: 0,
-		height: 0,
-		currentSlide: 0,
-	};
 
 	componentDidUpdate(prevProps) {
 		if (prevProps.results !== this.props.results) {
@@ -29,44 +22,33 @@ class SeriesResults extends React.Component {
 	}
 
 	render() {
-		return <div>
-			<Conditional if={this.props.isLoading}>
-				<div>
-					<h1>Series are loading</h1>
-				</div>
-			</Conditional>
-			<div className="container mb-2">
-				<Slider>
-					{this.props.results.map((result, index) => {
-						const {
-							image, title, description, _id,
-						} = result;
-						return (
-							// eslint-disable-next-line react/jsx-key,max-len
-							<Slide index={index}><SeriesResult key={_id} image={image} title={title} description={description} id={_id}/></Slide>
-						);
-					})}
-					<Slide index={-1}>
-						<div className="col">
-							<div className="card flex-md-row mb-4 box-shadow">
-								<img className="d-none d-md-block" src="https://pluspng.com/img-png/kitten-png--243.png" alt="sad kitten" style={{
-									width: '200px',
-									height: '250px',
-								}} />
-								<div className="card-body d-flex flex-column align-items-start">
-									<h3 className="mb-0">
+		return <Slider className="container">
+			{this.props.results.map((result, index) => {
+				const {
+					image, title, description, _id,
+				} = result;
+				return (
+					// eslint-disable-next-line react/jsx-key,max-len
+					<Slide index={index}><SeriesResult key={_id} image={image} title={title} description={description} id={_id}/></Slide>
+				);
+			})}
+			<Slide index={-1}>
+				<div className="col">
+					<div className="card flex-md-row mb-4 box-shadow">
+						<img className="d-none d-md-block" src="https://pluspng.com/img-png/kitten-png--243.png" alt="sad kitten" style={{
+							width: '200px',
+							height: '250px',
+						}} />
+						<div className="card-body d-flex flex-column align-items-start">
+							<h3 className="mb-0">
 											No more results
-									</h3>
-									<p className="card-text mb-auto">Try changing your search to find more results</p>
-								</div>
-							</div>
+							</h3>
+							<p className="card-text mb-auto">Try changing your search to find more results</p>
 						</div>
-					</Slide>
-				</Slider>
-			</div>
-			<ButtonBack className={classes.buttonBack}><i className={classes.left} /></ButtonBack>
-			<ButtonNext className={classes.buttonNext}><i className={classes.right} /></ButtonNext>
-		</div>;
+					</div>
+				</div>
+			</Slide>
+		</Slider>;
 	}
 }
 
