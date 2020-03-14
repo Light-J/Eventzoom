aa430f2231c82df62a20cbf8832bcd09f384295d
Light <Lightjp@cardiff.ac.uk>
2020-03-14 12:04:06 +0000

    Make attendees list nicer

--- react/src/components/AttendeesList.js
+++ react/src/components/AttendeesList.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Conditional from './Conditional';
 
 class AttendeesList extends Component {
 	static propTypes = {
 		eventId: PropTypes.string.isRequired,
 	};
 
+	state = {
+		attendees: [],
+	};
 
-	render = () => <div>
-		<ul className="list-group">
-			<li className="list-group-item">Cras justo odio</li>
-			<li className="list-group-item">Dapibus ac facilisis in</li>
-			<li className="list-group-item">Morbi leo risus</li>
-			<li className="list-group-item">Porta ac consectetur ac</li>
-			<li className="list-group-item">Vestibulum at eros</li>
-		</ul>
+
+	render = () => <div className="card mb-2">
+		<div className="card-header">Users attending</div>
+		<Conditional if={!this.state.attendees.length}>
+			<div className="card-body">
+				No attendees yet :(
+			</div>
+		</Conditional>
+		<Conditional if={this.state.attendees.length > 0}>
+			<ul className="list-group list-group-flush">
+				<li className="list-group-item">Cras justo odio</li>
+				<li className="list-group-item">Dapibus ac facilisis in</li>
+				<li className="list-group-item">Morbi leo risus</li>
+				<li className="list-group-item">Porta ac consectetur ac</li>
+				<li className="list-group-item">Vestibulum at eros</li>
+			</ul>
+		</Conditional>
 	</div>
 }
+
 
 export default AttendeesList;
