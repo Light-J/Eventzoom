7af6b2fc55f4503834837ced7a826d71bc71c252
Light <Lightjp@cardiff.ac.uk>
2020-03-18 12:43:26 +0000

    Fix linting on server

--- node/src/services/text.service.js
+++ node/src/services/text.service.js
@@ -1,6 +1,8 @@
 import twilio from 'twilio';
 import twilioConfig from '../../config/twilio';
 
+// This is from the twilio library so not much I can do about this
+// eslint-disable-next-line new-cap
 const client = new twilio(twilioConfig.account_sid, twilioConfig.auth_token);
 
 const sendText = async (to, body) => {
@@ -8,6 +10,6 @@ const sendText = async (to, body) => {
 		body,
 		to,
 		from: twilioConfig.number,
-	}).then((message) => console.log(message.sid));
+	});
 };
 export default { sendText };
