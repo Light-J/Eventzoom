f19772835cfe2c8521f9794a51cc0a5759ab2714
Hristo <hdimitrov@talkative.uk>
2020-03-24 07:46:57 +0000

    oauth

---
+++ node/src/controllers/zoom.controller.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import axios from 'axios';
+
+const clientId = 'tTT3scdPRMSFg8AyhPdwA';
+const clientSecret = 'gtIwfzHh5bgUsQ0w2l54ExS0Gja5Wqvd';
+const zoomAxios = axios.create({
+	auth: {
+		username: clientId,
+		password: clientSecret,
+	},
+});
+const redirectUrl = 'http://localhost:3001/zoom/auth';
+const router = express.Router();
+
+// looselsy based on https://github.com/zoom/zoom-oauth-sample-app
+// accessed 24 March 2020
+router.get(
+	'/',
+	async (req, res) => {
+		res.redirect(`https://zoom.us/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUrl}`);
+	},
+);
+
+router.get(
+	'/auth',
+	async (req, res) => {
+		console.log(req.query);
+		const url = `https://zoom.us/oauth/token?grant_type=authorization_code&code=${req.query.code}&redirect_uri=${redirectUrl}`;
+		const results = await zoomAxios.post(url);
+		// console.log(results.data);
+		let refresh1;
+		let refresh2;
+		try {
+			console.log(results.data.refresh_token);
+			refresh2 = await zoomAxios.post(`https://zoom.us/oauth/token?grant_type=refresh_token&refresh_token=${results.data.refresh_token}`);
+			// refresh1 = await zoomAxios.post(`https://zoom.us/oauth/token?grant_type=refresh_token&refresh_token=${results.data.refresh_token}`);
+		}
+		catch(e) {
+			console.log(e.response.data);
+		}
+		console.log(refresh2.data);
+		res.send('hello');
+	},
+);
+export default router;
