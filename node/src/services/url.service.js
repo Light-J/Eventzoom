784bc9530d408b31245bac111c8662894872423e
Hristo <hdimitrov@talkative.uk>
2020-03-25 21:02:12 +0000

    ~ changes URL shortener to fail gracefully

--- node/src/services/url.service.js
+++ node/src/services/url.service.js
@@ -3,12 +3,18 @@ import bitlyConfig from '../../config/bitly';
 
 
 const shorten = async (url) => {
-	const shortened = await axios.post('https://api-ssl.bitly.com/v4/shorten', { long_url: url }, {
-		headers: {
-			Authorization: `Bearer ${bitlyConfig.token}`,
-			'Content-Type': 'application/json',
-		},
-	});
+	let shortened;
+	try {
+		shortened = await axios.post('https://api-ssl.bitly.com/v4/shorten', { long_url: url }, {
+			headers: {
+				Authorization: `Bearer ${bitlyConfig.token}`,
+				'Content-Type': 'application/json',
+			},
+		});
+	} catch (e) {
+		// bit.ly is finnicky with localhost in its url
+		return url;
+	}
 	return shortened.data.link;
 };
 export default { shorten };
