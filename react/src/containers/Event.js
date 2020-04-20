da92639bda397115e0544e1cde97d58af27c6ec3
Alex <sibandaak@cardiff.ac.uk>
2020-04-20 10:40:36 +0100

    lint fixes

--- react/src/containers/Event.js
+++ react/src/containers/Event.js
@@ -12,6 +12,7 @@ import Conditional from '../components/Conditional';
 import SearchResults from '../components/SearchResults';
 import Attachment from '../components/Attachment';
 import RemindMeSwitch from '../components/RemindMeSwitch';
+import ShareIcon from './shareIcon';
 
 export class Event extends Component {
 	state = {
@@ -118,105 +119,111 @@ export class Event extends Component {
 
 	hasWindow = () => (typeof window !== 'undefined');
 
-	render = () => <div className='container'>
-		<div className="container">
-			<div className="card border-0 shadow my-5">
-				<Conditional if={this.state.error}>
-					<div className="card-body p-5">
+	render = () => {
+		this.state._id && this.makeShareText();  // eslint-disable-line
+		return <div className='container'>
+			<div className="container">
+				<div className="card border-0 shadow my-5">
+					<Conditional if={this.state.error}>
+						<div className="card-body p-5">
 
-						<div className="d-flex align-items-center">
-							<img src="https://pluspng.com/img-png/kitten-png--243.png" alt='Sad kitten'/>
-							<h1>Sorry this event could not be found</h1>
-						</div>
-					</div>
-				</Conditional>
-				<Conditional if={this.state.isLoaded}>
-					<div className="card-body p-5">
-						<h1 className="font-weight-light">{this.state.title}</h1>
-						<div className='row'>
-							<div className='col-md-7'>
-								<p>{this.state.attendeesAmount} out of {this.state.capacity} attending</p>
-								<p className="lead">{this.state.description}</p>
-								<img src={this.state.image} alt='Event' className="img-responsive mw-100"/>
+							<div className="d-flex align-items-center">
+								<img src="https://pluspng.com/img-png/kitten-png--243.png" alt='Sad kitten'/>
+								<h1>Sorry this event could not be found</h1>
 							</div>
-							<div className='col-md-5'>
-								<div className='card'>
-									<div className='card-body'>
-										<h5 className='card-title'>Event details</h5>
-										<DisabilityAccess disabilityAccess={this.state.disabilityAccess} />
-										<p>Date: {new Date(this.state.date).toLocaleString()}</p>
-										<p>Speaker: {this.state.speaker}</p>
-										<p>Location: {this.state.vagueLocation}</p>
-										{
-											this.state.organiser
-												? <p>Organiser:
-													<a href={`mailto:${this.state.organiser.email}`}>
-														{this.state.organiser.name || this.state.organiser.email}</a>
-												</p>
-												: <p>No organiser</p>
-										}
+						</div>
+					</Conditional>
+					<Conditional if={this.state.isLoaded}>
+						<div className="card-body p-5">
+							<h1 className="font-weight-light">{this.state.title}</h1>
+							<div className='row'>
+								<div className='col-md-7'>
+									<p>{this.state.attendeesAmount} out of {this.state.capacity} attending</p>
+									<p className="lead">{this.state.description}</p>
+									<img src={this.state.image} alt='Event' className="img-responsive mw-100"/>
+								</div>
+								<div className='col-md-5'>
+									<div className='card'>
+										<div className='card-body'>
+											<h5 className='card-title'>Event details</h5>
+											<DisabilityAccess disabilityAccess={this.state.disabilityAccess} />
+											<p>Date: {new Date(this.state.date).toLocaleString()}</p>
+											<p>Speaker: {this.state.speaker}</p>
+											<p>Location: {this.state.vagueLocation}</p>
+											{
+												this.state.organiser
+													? <p>Organiser:
+														<a href={`mailto:${this.state.organiser.email}`}>
+															{this.state.organiser.name || this.state.organiser.email}</a>
+													</p>
+													: <p>No organiser</p>
+											}
 
-										{
-											this.state.zoomUrl
-												? <span>
-													<p>Zoom: <a href={this.state.zoomUrl}>{this.state.zoomUrl}</a></p>
-													<p className="font-italic small">Click link above to join meeting</p>
-												</span>
-												: null
-										}
-										{
-											this.state.series
-												? <p>Series: <Link to={`/series/${this.state.series._id}`}>{this.state.series.title}</Link></p>
-												: <p>Not a part of a series</p>
-										}
-									</div>
-									<div className="card-footer text-center">
-										<AttendButton
-											full={this.state.capacity === this.state.attendeesAmount}
-											userAttending={this.state.attending}
-											price={this.state.price}
-											userCancelled={this.state.userCancelled}
-											onAttendChange={this.onAttendChange}
-											userReminding={this.state.reminding}
-											eventId={this.props.eventId}
-											date={this.state.date}
-										/>
-										<div className="mt-2">
-											<Conditional if={this.state.attending && this.props.user.phoneNumber}>
-												<RemindMeSwitch
-													reminding={this.state.reminding}
-													onRemindChange={this.remindMe} />
-											</Conditional>
+											{
+												this.state.zoomUrl
+													? <span>
+														<p>Zoom: <a href={this.state.zoomUrl}>{this.state.zoomUrl}</a></p>
+														<p className="font-italic small">Click link above to join meeting</p>
+													</span>
+													: null
+											}
+											{
+												this.state.series
+													? <p>Series: <Link to={`/series/${this.state.series._id}`}>{this.state.series.title}</Link></p>
+													: <p>Not a part of a series</p>
+											}
+										</div>
+										<div className="card-footer text-center">
+											<AttendButton
+												full={this.state.capacity === this.state.attendeesAmount}
+												userAttending={this.state.attending}
+												price={this.state.price}
+												userCancelled={this.state.userCancelled}
+												onAttendChange={this.onAttendChange}
+												userReminding={this.state.reminding}
+												eventId={this.props.eventId}
+												date={this.state.date}
+											/>
+											<div className="mt-2">
+												<Conditional if={this.state.attending && this.props.user.phoneNumber}>
+													<RemindMeSwitch
+														reminding={this.state.reminding}
+														onRemindChange={this.remindMe} />
+												</Conditional>
+											</div>
 										</div>
 									</div>
-								</div>
-								<Conditional if={this.state.attachments.length > 0}>
-									<div className="card mb-2 mt-2">
-										<div className="card-header">Attachments for this event</div>
-										<ul className="list-group list-group-flush">
-											{this.attachments()}
-										</ul>
+									<div>
+										<ShareIcon url={this.eventUrl} title={this.shareText} />
 									</div>
-								</Conditional>
-								<Conditional if={this.state.userOwner}>
-									<Link to={`/events/admin/${this.props.eventId}`} className="btn btn-primary mt-2 d-block">Go to admin view</Link>
-								</Conditional>
+									<Conditional if={this.state.attachments.length > 0}>
+										<div className="card mb-2 mt-2">
+											<div className="card-header">Attachments for this event</div>
+											<ul className="list-group list-group-flush">
+												{this.attachments()}
+											</ul>
+										</div>
+									</Conditional>
+									<Conditional if={this.state.userOwner}>
+										<Link to={`/events/admin/${this.props.eventId}`} className="btn btn-primary mt-2 d-block">Go to admin view</Link>
+									</Conditional>
+								</div>
 							</div>
+							<Conditional if={this.state.recommendations.length !== 0}>
+								<h3 className="m-2">You may also like</h3>
+								<SearchResults results={this.firstThreeRecommendations()} isLoading={false}/>
+							</Conditional>
+							<h3>Discussion board</h3>
+							<DiscussionEmbed
+								config={this.getDisqusConfig()}
+								shortname={this.state.disqusShortname}
+							/>
 						</div>
-						<Conditional if={this.state.recommendations.length !== 0}>
-							<h3 className="m-2">You may also like</h3>
-							<SearchResults results={this.firstThreeRecommendations()} isLoading={false}/>
-						</Conditional>
-						<h3>Discussion board</h3>
-						<DiscussionEmbed
-							config={this.getDisqusConfig()}
-							shortname={this.state.disqusShortname}
-						/>
-					</div>
-				</Conditional>
+					</Conditional>
+				</div>
 			</div>
-		</div>
-	</div>
+		</div>;
+	}
 }
 
 const mapStateToProps = (state) => ({
--- react/src/containers/shareIcon.js
+++ react/src/containers/shareIcon.js
@@ -1,56 +1,54 @@
-import React from "react";
+import React from 'react';
 import {
-  FacebookShareButton,
-  FacebookIcon,
-  TwitterShareButton,
-  TwitterIcon,
-  TumblrShareButton,
-  TumblrIcon,
-  WhatsappShareButton,
-  WhatsappIcon
-} from "react-share";
-import PropTypes from "prop-types";
-import "../scss/share.scss";
+	FacebookShareButton,
+	FacebookIcon,
+	TwitterShareButton,
+	TwitterIcon,
+	TumblrShareButton,
+	TumblrIcon,
+	WhatsappShareButton,
+	WhatsappIcon,
+} from 'react-share';
+import PropTypes from 'prop-types';
+import '../scss/share.scss';
 
 export default class ShareIcon extends React.Component {
-  static propTypes = {
-    url: PropTypes.string,
-	children: PropTypes.string,
-	title: PropTypes.string
-  };
+	static propTypes = {
+		url: PropTypes.string,
+		children: PropTypes.string,
+		title: PropTypes.string,
+	};
 
-  render() {
-    return (
-      <div className="share-container">
-        <p className="share-text">
-          Share your event via different social channels.
-        </p>
-        <div className="shareBtnWrap">
-          <FacebookShareButton title={this.props.title} url={this.props.url}>
-            {" "}
-            <span className="share-icon">
-              <FacebookIcon size={32} round={true} />
-            </span>
-          </FacebookShareButton>
-          <TwitterShareButton title={this.props.title} url={this.props.url}>
-            <span className="share-icon">
-              <TwitterIcon size={32} round={true} />
-            </span>
-          </TwitterShareButton>
-          <WhatsappShareButton title={this.props.title} url={this.props.url}>
-            {" "}
-            <span className="share-icon">
-              <WhatsappIcon size={32} round={true} />
-            </span>
-          </WhatsappShareButton>
-          <TumblrShareButton title={this.props.title} url={this.props.url}>
-            {" "}
-            <span className="share-icon">
-              <TumblrIcon size={32} round={true} />
-            </span>
-          </TumblrShareButton>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="share-container">
+				<p className="share-text">
+				Share your event via different social channels.
+				</p>
+				<div className="shareBtnWrap">
+					<FacebookShareButton title={this.props.title} url={this.props.url}>
+						{' '}
+						<span className="share-icon">
+							<FacebookIcon size={32} round={true} />
+						</span>
+					</FacebookShareButton>
+					<TwitterShareButton title={this.props.title} url={this.props.url}>
+						<span className="share-icon">
+							<TwitterIcon size={32} round={true} />
+						</span>
+					</TwitterShareButton>
+					<WhatsappShareButton title={this.props.title} url={this.props.url}>
+						{' '}
+						<span className="share-icon">
+							<WhatsappIcon size={32} round={true} /></span>
+					</WhatsappShareButton>
+					<TumblrShareButton title={this.props.title} url={this.props.url}>
+						{' '}
+						<span className="share-icon">
+							<TumblrIcon size={32} round={true} />
+						</span>
+					</TumblrShareButton>
+				</div>
+			</div>);
+	}
 }
